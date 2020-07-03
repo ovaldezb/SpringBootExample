9632fc7300db241793a0ed2d8300ea2bb4557c23
Omar Valdez <omar.valdez.becerril@gmail.com>
2020-07-03 18:32:39 -0500

    add registry-service app

---
+++ registry-service/src/main/java/mx/com/ovaldez/store/registry/RegistryServiceApplication.java
@@ -0,0 +1,15 @@
+package mx.com.ovaldez.store.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
