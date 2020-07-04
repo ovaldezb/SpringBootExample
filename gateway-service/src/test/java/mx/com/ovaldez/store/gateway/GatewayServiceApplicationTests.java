cbfeb6df01c081fcc60af87e853d7fa5f784a277
Omar Valdez <omar.valdez.becerril@gmail.com>
2020-07-03 21:16:36 -0500

    add gateway-service

---
+++ gateway-service/src/test/java/mx/com/ovaldez/store/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.ovaldez.store.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
